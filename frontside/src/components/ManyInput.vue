3cfc6f1b08d23af4241ef1f5338a8a7c81658fef
GimaYuto <yurihanagima@gmail.com>
2022-03-30 21:55:40 +0900

    多段入力のエラー修正と実験

--- frontside/src/components/ManyInput.vue
+++ frontside/src/components/ManyInput.vue
@@ -1,43 +1,45 @@
 <template>
     <div id="ManyInput">
-        {{message}}
-        <!-- <form v-for="word in words" :key="word.id">
+        <h1>多段入力</h1>
+        <p>{{message}}</p>
+        <form v-for="word in words" :key="word.id">
             <p>{{word.id}}</p>
             <input type="text" v-model="word.name">
-            <button type="button" @click="manyPush(words)">提出</button>
-            <button type="button" @click="addInput()">像か</button> -->
-        <!-- </form> -->
+        </form>
+        <button type="button" @click="addInput()">像か</button>
+        <button type="button" @click="manyPush(words)">提出</button>
     </div>
 </template>
 
 <script>
 export default {
-    name:"ManyInput",
-    deta(){
+    data(){
         return {
-            // words:[{
-            //     id:0,
-            //     name:null,
-            // }],
-            // num:0,
-            message:"複数同時入力のテストです。"
+            words:[{
+                id:0,
+                name:null,
+            }],
+            num:0,
+            message:"manyInput"
         }
     },
     mounted(){
         console.log("manyInput")
+        this.message="ManyInput"
+        console.log(this.message)
     },
-    // methods:{
-    //     addInput(){
-    //         this.num = this.num + 1
-    //         let addContent={
-    //             id:this.num,
-    //             name:''
-    //         }
-    //         this.wrods.push(addContent)
-    //     },
-    //     manyPush(data){
-    //         console.log(data)
-    //     }
-    // }
+    methods:{
+        addInput(){
+            this.num = this.num + 1
+            let addContent={
+                id:this.num,
+                name:''
+            }
+            this.words.push(addContent)
+        },
+        manyPush(data){
+            console.log(data)
+        }
+    }
 }
 </script>
