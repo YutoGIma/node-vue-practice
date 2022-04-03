6ae2df4c0e3badb7239a648b38eb242d7381029f
GimaYuto <yurihanagima@gmail.com>
2022-04-03 21:20:27 +0900

    メールアドレスのバリデーション実装

--- frontside/src/components/ManyInput.vue
+++ frontside/src/components/ManyInput.vue
@@ -6,6 +6,8 @@
             <p>{{word.id}}</p>
             <input type="text" v-model="word.name">
             <p v-if="showNameError(word.id)">{{nameError}}</p>
+            <input type="text" v-model="word.email">
+            <p v-if="showEmailError(word.id)">{{emailError}}</p>
         </form>
         <button type="button" @click="addInput()">像か</button>
         <button type="button" @click="manyPush(words)">提出</button>
@@ -23,12 +25,13 @@ export default {
             num:0,
             message:"manyInput",
             nameError:"",
+            emailError:"",
         }
     },
     computed: {
         showNameError(){
             return function (id){
-                if(this.words[0].name == null){
+                if(this.words[id].name == null){
                     return false
                 }
                 let inputName = this.words[id].name
@@ -37,6 +40,27 @@ export default {
                     return true
                 }else if(inputName.length < 4){
                     this.nameError = "4文字以上入力して下さい"
+                    return true
+                }else if(inputName.length > 20){
+                    this.nameError = "20文字以下で入力して下さい"
+                    return true
+                }else{
+                    return false
+                }
+            }
+        },
+        showEmailError(){
+            return function(id){
+                if(this.words[id].email == null){
+                    return false
+                }
+                let inputEmail = this.words[id].email
+                let emailChecker = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]+\.[A-Za-z0-9]+$/
+                if(!emailChecker.test(inputEmail)){
+                    this.emailError = "メールアドレスを正しく入力してください"
+                    return true
+                }else if(inputEmail.length < 4){
+                    this.emailError = "4文字以上入力して下さい"
                     return true
                 }else{
                     return false
@@ -54,7 +78,8 @@ export default {
             this.num = this.num + 1
             let addContent={
                 id:this.num,
-                name:''
+                name:'',
+                email:''
             }
             this.words.push(addContent)
         },
@@ -64,3 +89,27 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+input{
+    display:block;
+    margin:10px auto 0px;
+}
+p{
+    margin:0;
+}
+button{
+    margin:15px 5px;
+    font-size:18px;
+    border:none;
+    padding:5px 10px;
+    background-color:rgb(63, 63, 63);
+    color:#fff;
+    border-radius:10px;
+}
+form{
+    border: #000 3px solid;
+    margin:20px;
+    padding:20px;
+}
+</style>
